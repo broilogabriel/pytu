d3dc4f6423b244e49599b825c495a77d3712dc4f
broilogabriel <broilo.gabriel@gmail.com>
2017-12-11 23:14:19 +0000

    feat: add basic argcomplete

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+#!/usr/bin/env python
+# PYTHON_ARGCOMPLETE_OK
+
 import argparse
+
+import argcomplete
+
+
+def main(arg):
+    print(arg)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("cmd", help="start | stop | status | show")
+    parser.add_argument("cmd", choices=["start", "stop", "status", "show"])
+    argcomplete.autocomplete(parser)
     args = parser.parse_args()
+    main(args.cmd)
